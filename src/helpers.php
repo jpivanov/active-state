5bedd293e812b60f7f793f56f63c01d3381e56ca
Julian Ivanov <juliani@perfectomobile.com>
2016-12-15 09:05:08 +0200

    Add support for passing array of links to active_check

--- src/helpers.php
+++ src/helpers.php
@@ -1,22 +1,29 @@
 <?php
 
 
-    /**
-     * Generate a HTML link to a controller action.
-     *
-     * @param string $url
-     * @param string $deep
-     * @param array  $active
-     * @param array  $inactive
-     *
-     * @return string
-     */
-     if (! function_exists('active_check')) {
+/**
+ * Generate a HTML link to a controller action.
+ *
+ * @param string $url
+ * @param string $deep
+ * @param array $active
+ * @param array $inactive
+ *
+ * @return string
+ */
+if (!function_exists('active_check')) {
 
-     	function active_check($url,$deep=false,$active=null,$inactive=null)
-     	{
-     		return app('active-state')->check($url, $deep, $active, $inactive);   	
-          }
-     }
+    function active_check($url, $deep = false, $active = null, $inactive = null)
+    {
+        if (is_array($url)) {
+            $state = '';
+            foreach ($url as $node) {
+                $state .= app('active-state')->check($node, $deep, $active, $inactive) . ' ';
+            }
 
+            return $state;
+        }
 
+        return app('active-state')->check($url, $deep, $active, $inactive);
+    }
+}
